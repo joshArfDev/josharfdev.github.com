910edee8357ac1241c87bd34f7b82407416d1e81
Joshua_Arfa <soondevjosh@gmail.com>
2022-11-01 14:04:09 -0600

    Redireccionar 2da vez

--- boletines/js/main.js
+++ boletines/js/main.js
@@ -19,7 +19,7 @@ links.forEach(function(link){
         content.classList.add('animated');
 
         setTimeout(function(){ 
-            location.href = '/boletines'; 
+            location.href = "/boletines"; 
         },600)
         
 
