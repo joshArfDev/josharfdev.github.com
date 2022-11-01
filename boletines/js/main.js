48edff44b0309ca91107574200fab011841cbba6
Joshua_Arfa <soondevjosh@gmail.com>
2022-11-01 13:38:21 -0600

    Agregamos el proyecto Boletines

---
+++ boletines/js/main.js
@@ -0,0 +1,32 @@
+//Obtener los elementos de la clase .close
+
+let links = document.querySelectorAll('.close');
+
+//Recorrerlos
+links.forEach(function(link){
+
+    //Agregar un evento click a cada uno de ellas
+    link.addEventListener('click', function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+
+        //Quitarle las clases de animación que ya tiene
+        content.classList.remove('fadeInDown');
+        content.classList.remove('animated');
+
+        //Agregarle clases para su salida fadeOutUp
+        content.classList.add('fadeOutUp');
+        content.classList.add('animated');
+
+        setTimeout(function(){
+            location.href = '/';
+        },600)
+        
+
+        return false;
+    })
+
+})
+
+ 
+
